4d3e00c0d9551481dfce34532ba234a622f40547
Anthony Gufron <anthonygfrn@gmail.com>
2022-06-22 11:14:26 +0800

    Models Setup

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,6 +1,34 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+
+#################################
+### DATABASE SETUPS ############
+###############################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://anthony:antpassword@localhost/restaurants_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+###########################
+#### LOGIN CONFIGS #######
+#########################
+
+login_manager = LoginManager()
+
+# We can now pass in our app to the login manager
+login_manager.init_app(app)
+
+# Tell users what view to go to when they need to login.
+login_manager.login_view = "users.login"
+
 
 from puppycompanyblog.core.views import core
 app.register_blueprint(core)
